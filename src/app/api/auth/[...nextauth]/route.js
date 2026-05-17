7b5275f6ade0ee8b10d218e917564d0efeb6a934
Sandesh660 <sandeshkumar2752@gmail.com>
2026-05-18 00:08:36 +0500

    fix: history and login work

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
 import GitHubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/db"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
