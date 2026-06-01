bfa62251e5f5b3393ae24951424b1d0456575f48
Sandesh660 <sandeshkumar2752@gmail.com>
2026-06-01 20:52:10 +0500

    remove: remove dead section block component

--- src/components/SectionBlock.jsx
+++
@@ -1,8 +0,0 @@
-export default function SectionBlock({ title, children }) {
- return (
-    <div className="border p-4 rounded-lg">
-        <h3 className="font-bold mb-2">{title}</h3>
-        <div>{children}</div>
-    </div>
- )   
-}
