d38624122c11c4be41dffc4f252fbaa015926b67
Sandesh660 <sandeshkumar2752@gmail.com>
2026-06-11 21:28:46 +0500

    fix: fix auth middlware matcher paths

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,11 +1,11 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
-    pages: {
-        signIn: "/login"
-    }
-})
+  pages: {
+    signIn: "/login",
+  },
+});
 
 export const config = {
-    matcher: ["/"]
-}
+  matcher: ["/", "/history/:path*", "/irt/:path*"],
+};
